d9fa3f063ceb27949eccc1e817762cd6af423d61
srinivass2000 <anishtalauliker@gmail.com>
2021-07-06 09:39:03 +0530

    Autentication

--- src/Security/LoginFormAutenticatorAuthenticator.php
+++ src/Security/LoginFormAutenticatorAuthenticator.php
@@ -51,7 +51,7 @@ class LoginFormAutenticatorAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('home'));
+        return new RedirectResponse($this->urlGenerator->generate('app_login'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
