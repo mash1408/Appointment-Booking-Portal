f851262f88a0b3f801971f8172231a6c52ac98ee
srinivass2000 <anishtalauliker@gmail.com>
2021-07-07 15:22:34 +0530

    Database seeder successfully added --Admin

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
+class AppFixtures extends Fixture
+{ 
+        private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    // ...
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setEmail('admin@admin.com');
+
+        $password = $this->encoder->encodePassword($user, 'password');
+        $user->setPassword($password);
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
