606ae2e0db4ed7a9f24056a2983419f46011a50b
Sherwin Fernandes <sherwfernand@gmail.com>
2021-07-08 14:32:30 +0530

    Added controller to get slots

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Slot;
+use Doctrine\ORM\Mapping\Id;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        $slots = $this->getDoctrine()->getRepository(Slot::class)->findAll();
+        // return $this->render('home/index.html.twig', [
+        //     'controller_name' => 'HomeController',
+        // ]);
+
+        $slotArray = [];
+
+        foreach ($slots as $slot) {
+            array_push($slotArray, [$slot->getId(),$slot->getSlotDate()->format('Y-m-d'), $slot->getSlotTime()->format('H:i'), $slot->getBooked()]);
+        }
+
+        return $this->render('home/index.html.twig', ['slots' => $slotArray]);
     }
 }
