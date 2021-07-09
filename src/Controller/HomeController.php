a1c44ca119950faf058a37e4daa84f5aeac7ea74
Sherwin Fernandes <sherwfernand@gmail.com>
2021-07-09 12:53:19 +0530

    Configured routes for home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,11 +6,34 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Slot;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Doctrine\ORM\Mapping\Id;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home/{date}', name: 'home')]
+    #[Route('/home', name: 'home')]
+    public function calender(Request $request): Response
+    {
+        $form = $this->createFormBuilder()
+            ->add('slotDate', DateType::class)
+            ->add('save', SubmitType::class, ['label' => 'View Slots'])
+            ->getForm();
+
+        // Submitting the form
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            $date = (string)$form->getData()["slotDate"]->format('Y-m-d');
+            return $this->redirectToRoute('slotlist',array('date'=>$date ));
+        }
+
+        return $this->render('home/calendar.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/home/{date}', name: 'slotlist')]
     public function index($date = 'date'): Response
     {
         $dateobj =  \DateTime::createFromFormat("Y-m-d",$date);
